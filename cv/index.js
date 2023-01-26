d951dad17898efc508bdb75f48196eb6d2b2608d
MerlyBriz <122127175+MerlyBriz@users.noreply.github.com>
2023-01-26 18:40:54 -0300

    Add files via upload

---
+++ cv/index.js
@@ -0,0 +1,16 @@
+var text = document.getElementById("master-text");
+var str = text.innerHTML;
+
+text.innerHTML= "";
+
+var speed = 200;
+var i = 0
+
+function typeWriter (){
+    if (i < str.length){
+        text.innerHTML += str.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
+setTimeout(typeWriter, speed)
